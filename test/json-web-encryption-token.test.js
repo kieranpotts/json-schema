e0526fe8f848245350027a60d3fa84307a825ccf
Kieran Potts <hello@kieranpotts.com>
2025-05-20 08:34:07 +0100

    dev: extend tests and docs for jwe token schema

---
+++ test/json-web-encryption-token.test.js
@@ -0,0 +1,34 @@
+import schema from '../src/json-web-encryption-token.schema.json'
+import Ajv from 'ajv/dist/2020'
+
+describe('JSON Web Encryption (JWE) token', () => {
+  let ajv
+
+  beforeAll(() => {
+    ajv = new Ajv()
+  })
+
+  test('validates a valid JWE token', () => {
+    /* Valid JWE token example taken from:
+    https://datatracker.ietf.org/doc/html/rfc7516#section-3.3 */
+    const data = 'eyJhbGciOiJSU0EtT0FFUCIsImVuYyI6IkEyNTZHQ00ifQ.OKOawDo13gRp2ojaHV7LFpZcgV7T6DVZKTyKOMTYUmKoTCVJRgckCL9kiMT03JGeipsEdY3mx_etLbbWSrFr05kLzcSr4qKAq7YN7e9jwQRb23nfa6c9d-StnImGyFDbSv04uVuxIp5Zms1gNxKKK2Da14B8S4rzVRltdYwam_lDp5XnZAYpQdb76FdIKLaVmqgfwX7XWRxv2322i-vDxRfqNzo_tETKzpVLzfiwQyeyPGLBIO56YJ7eObdv0je81860ppamavo35UgoRdbYaBcoh9QcfylQr66oc6vFWXRcZ_ZT2LawVCWTIy3brGPi6UklfCpIMfIjf7iGdXKHzg.48V1_ALb6US04U3b.5eym8TW_c8SuK0ltJ3rpYIzOeDQz7TALvtu6UG9oMo4vpzs9tX_EFShS8iB7j6jiSdiwkIr3ajwQzaBtQD_A.XFBoMYUZodetZdvTiFvSkQ'
+
+    const validate = ajv.compile(schema)
+    const isValid = validate(data)
+
+    expect(isValid).toBe(true)
+    expect(validate.errors).toBeNull()
+  })
+
+  const invalid_data = [
+    'invalid-token',
+  ]
+
+  test.each(invalid_data)('invalidates an invalid JWE token: %p', (data) => {
+    const validate = ajv.compile(schema)
+    const isValid = validate(data)
+
+    expect(isValid).toBe(false)
+    expect(validate.errors).not.toBeNull()
+  })
+})
